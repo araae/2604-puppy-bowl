d703eebb8c26723aa2da02507141e247d74b4872
araae <araliae@icloud.com>
2026-05-08 15:21:48 -0700

    added getPuppies and getPuppy functions

--- index.js
+++ index.js
@@ -16,14 +16,40 @@ let puppies = [];
 let selectedPuppy;
 let teams = [];
 
-//define a asyncronous function named getPuppies
+//define an asyncronous function named getPuppies
 //implement try/catch to execute the code block and catch possible errors instead of crashing
-//
+//request the list of players from the API and await the response
+//parse the response as JSON and await the result
+//reassign puppies to the array of players from the API responses
+//re-render the page
+//catch any error within the try block
+//log the error in the console
 async function getPuppies() {
   try {
     const response = await fetch(API + "/players");
     const result = await response.json();
-    puppies = result.data;
+    puppies = result.data.players;
+    render();
+  } catch (woof) {
+    console.error(woof);
+  }
+}
+
+//i wasnt sure if i should be reusing/re explaining pseudocodes, but i did it just in case
+
+//define an asyncronous function named getPuppy that fetches a specific id from the API
+//implement try/catch to execute the code block and catch possible errors instead of crashing
+//request the specific player id from the API await the response
+//parse the response as JSON and await the result
+//Reassign selectedPuppy to the player object from the API response
+//re-render the page
+//catch any error within the try block
+//log the error in the console
+async function getPuppy(id) {
+  try {
+    const response = await fetch(API + "/players/" + id);
+    const result = await response.json();
+    selectedPuppy = result.data.player;
     render();
   } catch (woof) {
     console.error(woof);
