2147d832ac9ddab03d6d37efc4c6b3b027e185d3
araae <araliae@icloud.com>
2026-05-08 16:20:12 -0700

    added addPuppy and removePuppy functions

--- index.js
+++ index.js
@@ -55,3 +55,50 @@ async function getPuppy(id) {
     console.error(woof);
   }
 }
+
+//define an asyncronous function named addPuppy that takes a puppy object as input
+//implement try/catch to execute the code block and catch possible errors instead of crashing
+//send a POST request using fetch to the players endpoint with the puppy data
+//set the method to post(add new data)
+//set the headers content type in JSON format
+//convert the data (puppy object) into a JSON string
+//parse the response as JSON and await the result
+//log the result to the console for debugging
+//wait for puppy list to get reloaded from the API
+//catch any error within the try block
+//log the error in the console
+
+async function addPuppy(puppy) {
+  try {
+    const response = await fetch(API + "/players", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(puppy),
+    });
+    const result = await response.json();
+    console.debug(result);
+    await getPuppies();
+  } catch (woof) {
+    console.error(woof);
+  }
+}
+
+//on second thought im gonna skip writing a pseudocode for try and catch over and over (•ᴗ•")
+//i keep overthinking this T_T
+
+//define an asyncronous function named removePuppy that takes an id as input
+//send a delete request using fetch to players to remove the puppy
+//set the method to delete
+//reset the selected puppy that no longer exists to null
+//wait for puppy list to get reloaded from API
+async function removePuppy(id) {
+  try {
+    await fetch(API + "/player/" + id, {
+      method: "DELETE",
+    });
+    selectedPuppy = null;
+    await getPuppies();
+  } catch (woof) {
+    console.error(woof);
+  }
+}
